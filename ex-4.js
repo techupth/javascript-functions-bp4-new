10b0f4763426e9723ef6352053c4dee97348a680
Wasin <wasin1026@gmail.com>
2024-06-03 20:04:03 +0700

    Exercise #4 : Grouping the Function with Object

--- ex-4.js
+++ ex-4.js
@@ -1,19 +1,11 @@
 // Start coding here
 let calculator = {
-  add: function add(a, b) {
-    return a + b;
-  },
-  subtract: function subtract(a, b) {
-    return a - b;
-  },
-  multiply: function multiply(a, b) {
-    return a * b;
-  },
-  divide: function divide(a, b) {
-    return a / b;
-  },
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
 };
-let addResult=calculator.add(10,20);
+let addResult = calculator.add(10, 20);
 console.log(addResult);
-let divideResult=calculator.divide(3000,10);
+let divideResult = calculator.divide(3000, 10);
 console.log(divideResult);
